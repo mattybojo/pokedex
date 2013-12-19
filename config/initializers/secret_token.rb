4b3e4d055268a0465621d25e25dd3284f64fadb8
Matt Bojorquez <mattbojorquez@gmail.com>
2013-12-18 22:37:53 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pokedex::Application.config.secret_key_base = 'bdbd49af52665715973fcccbd748e753f161d8097897565faa64b50790ac60a78dad215d65835a40dffb55b1cac50cd332c54db7bce72d95a930d74c8827d2b3'
